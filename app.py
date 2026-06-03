67a2eeca559867c69f213e13236e6719ee389ced
Sathvika-collab-debug <sathvikamugudhanam666@gmail.com>
2026-06-03 08:13:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+st.set_page_config(page_title="Loan App", layout="wide")
+
+st.title("🏦 Loan Approval System")
+
+income = st.number_input("Applicant Income", 0)
+loan_amount = st.number_input("Loan Amount", 0)
+
+if st.button("Check Eligibility"):
+
+    if income >= loan_amount:
+        st.success("✅ APPROVED")
+    else:
+        st.error("❌ REJECTED")
