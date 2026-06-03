ca72755c52a75c200c102b63d49c35966762090e
Sathvika-collab-debug <sathvikamugudhanam666@gmail.com>
2026-06-03 08:40:54 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Bank Loan Dashboard", layout="wide")
 
 st.title("🏦 Bank Loan Approval Dashboard")
+st.subheader("Smart Loan Eligibility Checker")
 
 st.markdown("---")
 
@@ -11,13 +12,13 @@ gender = st.selectbox("Gender", ["Male", "Female"])
 married = st.selectbox("Married", ["Yes", "No"])
 education = st.selectbox("Education", ["Graduate", "Not Graduate"])
 
-income = st.number_input("Applicant Income", min_value=0)
-loan_amount = st.number_input("Loan Amount", min_value=0)
-interest = st.number_input("Rate of Interest (%)", min_value=0.0, value=8.5)
+income = st.number_input("Applicant Income", 0)
+loan_amount = st.number_input("Loan Amount", 0)
+interest = st.number_input("Rate of Interest (%)", value=8.5)
 
 st.markdown("---")
 
-# Logic (SAME RULE)
+# Logic (same rule)
 if st.button("Check Eligibility"):
 
     if income >= loan_amount:
